88f2197c0b054c2ee1a8dc864be0535f87e11725
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-21 17:34:34 +0100

    clean

--- fbcp-ili9341.cpp
+++ fbcp-ili9341.cpp
@@ -25,61 +25,12 @@
 #include "mailbox.h"
 #include "diff.h"
 
-int CountNumChangedPixels(uint16_t *framebuffer, uint16_t *prevFramebuffer)
-{
-  int changedPixels = 0;
-  for (int y = 0; y < DISPLAY_HEIGHT; ++y)
-  {
-    for (int x = 0; x < DISPLAY_WIDTH; ++x)
-      if (framebuffer[x] != prevFramebuffer[x])
-        ++changedPixels;
-
-    framebuffer += gpuFramebufferScanlineStrideBytes >> 1;
-    prevFramebuffer += gpuFramebufferScanlineStrideBytes >> 1;
-  }
-  return changedPixels;
-}
-
-volatile bool programRunning = true;
-
-void MarkProgramQuitting()
-{
-  programRunning = false;
-}
-
-void ProgramInterruptHandler(int signal)
-{
-  printf("Signal (%d) received, quitting\n", signal);
-  static int quitHandlerCalled = 0;
-  if (++quitHandlerCalled >= 5)
-  {
-    printf("Ctrl-C handler invoked five times, looks like fbcp-ili9341 is not gracefully quitting - performing a forcible shutdown!\n");
-    exit(1);
-  }
-  MarkProgramQuitting();
-  __sync_synchronize();
-  // Wake the SPI thread if it was sleeping so that it can gracefully quit
-  if (spiTaskMemory)
-  {
-    __atomic_fetch_add(&spiTaskMemory->queueHead, 1, __ATOMIC_SEQ_CST);
-    __atomic_fetch_add(&spiTaskMemory->queueTail, 1, __ATOMIC_SEQ_CST);
-    syscall(SYS_futex, &spiTaskMemory->queueTail, FUTEX_WAKE, 1, 0, 0, 0); // Wake the SPI thread if it was sleeping to get new tasks
-  }
-
-}
-
 int main()
 {
-  signal(SIGINT, ProgramInterruptHandler);
-  signal(SIGQUIT, ProgramInterruptHandler);
-  signal(SIGUSR1, ProgramInterruptHandler);
-  signal(SIGUSR2, ProgramInterruptHandler);
-  signal(SIGTERM, ProgramInterruptHandler);
-
-  // OpenMailbox();
+  OpenMailbox();
   InitSPI();
 
   DeinitSPI();
-  // CloseMailbox();
+  CloseMailbox();
   printf("Quit.\n");
 }
