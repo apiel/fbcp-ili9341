c8dc9bd758800661fb8b8f32d3714b8d1a7134e0
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-21 19:45:10 +0100

    test

--- display.cpp
+++ display.cpp
@@ -16,21 +16,37 @@ void drawPixel(uint16_t x, uint16_t y, uint16_t color)
 {
   sendAddr(DISPLAY_SET_CURSOR_X, (uint16_t)x, (uint16_t)x);
   sendAddr(DISPLAY_SET_CURSOR_Y, (uint16_t)y, (uint16_t)y);
-  
+
   uint8_t data[2] = {color >> 8, color & 0xFF};
   sendCmd(DISPLAY_WRITE_PIXELS, data, 2);
 }
 
+void drawFillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color)
+{
+  sendAddr(DISPLAY_SET_CURSOR_X, (uint16_t)x, (uint16_t)(x + w - 1));
+  sendAddr(DISPLAY_SET_CURSOR_Y, (uint16_t)y, (uint16_t)(y + h - 1));
+
+  uint16_t size = w * h * SPI_BYTESPERPIXEL;
+  uint8_t pixels[size];
+  uint8_t pixel[2] = {color >> 8, color & 0xFF};
+  for (uint16_t i = 0; i < size; i += SPI_BYTESPERPIXEL)
+  {
+    memcpy(pixels + i, pixel, SPI_BYTESPERPIXEL);
+  }
+  sendCmd(DISPLAY_WRITE_PIXELS, pixels, size);
+}
+
 void ClearScreen()
 {
-  sendAddr(DISPLAY_SET_CURSOR_X, 0, DISPLAY_WIDTH - 1);
-  sendAddr(DISPLAY_SET_CURSOR_Y, 0, DISPLAY_HEIGHT - 1);
+  // sendAddr(DISPLAY_SET_CURSOR_X, 0, DISPLAY_WIDTH - 1);
+  // sendAddr(DISPLAY_SET_CURSOR_Y, 0, DISPLAY_HEIGHT - 1);
 
-  uint16_t pixel = 0xFF00FF;
-  uint16_t size = DISPLAY_WIDTH * DISPLAY_HEIGHT * SPI_BYTESPERPIXEL;
-  uint8_t pixels[size];
-  memset(pixels, (uint8_t)0, size);
-  sendCmd(DISPLAY_WRITE_PIXELS, pixels, size);
+  // uint16_t size = DISPLAY_WIDTH * DISPLAY_HEIGHT * SPI_BYTESPERPIXEL;
+  // uint8_t pixels[size];
+  // memset(pixels, (uint8_t)0, size);
+  // sendCmd(DISPLAY_WRITE_PIXELS, pixels, size);
+
+  drawFillRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0x00FF);
 
   SPI_TRANSFER(DISPLAY_SET_CURSOR_X, 0, 0, (DISPLAY_WIDTH - 1) >> 8, (DISPLAY_WIDTH - 1) & 0xFF);
   SPI_TRANSFER(DISPLAY_SET_CURSOR_Y, 0, 0, (DISPLAY_HEIGHT - 1) >> 8, (DISPLAY_HEIGHT - 1) & 0xFF);
