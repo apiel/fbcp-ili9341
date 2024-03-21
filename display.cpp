cb023ec6db078a309f3906f8067a15f78be0ed2f
Alexandre Piel <alexandre.piel@zooplus.com>
2024-03-21 20:06:46 +0100

    test

--- display.cpp
+++ display.cpp
@@ -55,18 +55,28 @@ void ClearScreen()
   // memset(pixels, (uint8_t)0, size);
   // sendCmd(DISPLAY_WRITE_PIXELS, pixels, size);
 
-  for (int y = 0; y < DISPLAY_HEIGHT; ++y)
-  {
-    SPI_TRANSFER(DISPLAY_SET_CURSOR_X, 0, 0, (DISPLAY_WIDTH - 1) >> 8, (DISPLAY_WIDTH - 1) & 0xFF);
-    SPI_TRANSFER(DISPLAY_SET_CURSOR_Y, (uint8_t)(y >> 8), (uint8_t)(y & 0xFF), (DISPLAY_HEIGHT - 1) >> 8, (DISPLAY_HEIGHT - 1) & 0xFF);
+  // for (int y = 0; y < DISPLAY_HEIGHT; ++y)
+  // {
+  //   SPI_TRANSFER(DISPLAY_SET_CURSOR_X, 0, 0, (DISPLAY_WIDTH - 1) >> 8, (DISPLAY_WIDTH - 1) & 0xFF);
+  //   SPI_TRANSFER(DISPLAY_SET_CURSOR_Y, (uint8_t)(y >> 8), (uint8_t)(y & 0xFF), (DISPLAY_HEIGHT - 1) >> 8, (DISPLAY_HEIGHT - 1) & 0xFF);
 
-    SPITask *clearLine = AllocTask(DISPLAY_WIDTH * SPI_BYTESPERPIXEL);
-    clearLine->cmd = DISPLAY_WRITE_PIXELS;
-    memset(clearLine->data, 0, clearLine->size);
-    CommitTask(clearLine);
-    RunSPITask(clearLine);
-    DoneTask(clearLine);
-  }
+  //   SPITask *clearLine = AllocTask(DISPLAY_WIDTH * SPI_BYTESPERPIXEL);
+  //   clearLine->cmd = DISPLAY_WRITE_PIXELS;
+  //   memset(clearLine->data, 0, clearLine->size);
+  //   CommitTask(clearLine);
+  //   RunSPITask(clearLine);
+  //   DoneTask(clearLine);
+  // }
+
+  SPI_TRANSFER(DISPLAY_SET_CURSOR_X, 0, 0, (DISPLAY_WIDTH - 1) >> 8, (DISPLAY_WIDTH - 1) & 0xFF);
+  SPI_TRANSFER(DISPLAY_SET_CURSOR_Y, 0, 0, (DISPLAY_HEIGHT - 1) >> 8, (DISPLAY_HEIGHT - 1) & 0xFF);
+
+  SPITask *clearLine = AllocTask(DISPLAY_WIDTH * DISPLAY_HEIGHT * SPI_BYTESPERPIXEL);
+  clearLine->cmd = DISPLAY_WRITE_PIXELS;
+  memset(clearLine->data, 0, clearLine->size);
+  CommitTask(clearLine);
+  RunSPITask(clearLine);
+  DoneTask(clearLine);
 
   SPI_TRANSFER(DISPLAY_SET_CURSOR_X, 0, 0, (DISPLAY_WIDTH - 1) >> 8, (DISPLAY_WIDTH - 1) & 0xFF);
   SPI_TRANSFER(DISPLAY_SET_CURSOR_Y, 0, 0, (DISPLAY_HEIGHT - 1) >> 8, (DISPLAY_HEIGHT - 1) & 0xFF);
